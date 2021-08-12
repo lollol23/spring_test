cad1081783e353034106cf28b097a159b28a10e0
lollol23 <mark6262@naver.com>
2021-08-12 14:54:18 +0900

    [임윤섭] -gradle jsp 연동및 test03풀이 추가.

---
+++ src/main/java/com/lollol23/test/lesson01/Test03.java
@@ -0,0 +1,12 @@
+package com.lollol23.test.lesson01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test03 {
+	@RequestMapping("/lesson01/test03/1")
+	public String view() {
+		return "lesson01/test03";
+	}
+}
